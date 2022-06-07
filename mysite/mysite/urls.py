1b3a35089778505b86a95782c19fe2bd67f13457
Nastya <Golub.Nastusha@yandex.ru>
2022-06-07 23:16:46 +0300

    9

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include('blog.urls', namespace='blog'))
+    path("blog/", include('blog.urls', namespace='blog')),
+    path("", include('blog.urls', namespace='blog'))
 ]
